407f980513caa7fa2d2913df44ad821d8410e1f6
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2021-08-12 19:47:34 +0700

    refactor: refactoring Error Handling code using extention function

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -1,10 +1,10 @@
-const ClientError = require('../../api/exceptions/ClientError');
+const ClientError = require('../exceptions/ClientError');
 
 class SongsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
-  
+
     this.postSongHandler = this.postSongHandler.bind(this);
     this.getSongsHandler = this.getSongsHandler.bind(this);
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
@@ -13,43 +13,23 @@ class SongsHandler {
   }
 
   async postSongHandler(request, h) {
-    try {
-      this._validator.validateSongPayload(request.payload);
-      const {
-        title = 'untitled', year, performer, genre, duration,
-      } = request.payload;
+    this._validator.validateSongPayload(request.payload);
+    const {
+      title = 'untitled', year, performer, genre, duration,
+    } = request.payload;
 
-      const songId = await this._service.addSong({
-        title, year, performer, genre, duration,
-      });
-      const response = h.response({
-        status: 'success',
-        message: 'Lagu berhasil ditambahkan',
-        data: {
-          songId,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (err) {
-      if (err instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: err.message,
-        });
-        response.code(err.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(err);
-      return response;
-    }
+    const songId = await this._service.addSong({
+      title, year, performer, genre, duration,
+    });
+    const response = h.response({
+      status: 'success',
+      message: 'Lagu berhasil ditambahkan',
+      data: {
+        songId,
+      },
+    });
+    response.code(201);
+    return response;
   }
 
   async getSongsHandler() {
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const songs = require('./api/songs');
 const SongService = require('./api/services/postgres/SongServices');
 const SongValidator = require('./api/validator/songs');
+const ClientError = require('./api/exceptions/ClientError');
 
 const init = async () => {
   const songService = new SongService();
@@ -23,6 +24,30 @@ const init = async () => {
       service: songService,
       validator: SongValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    if (response instanceof Error) {
+      const newResponse = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    return response.continue || response;
   });
 
   await server.start();
